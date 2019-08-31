6192d25fbe01a8bd33f3c68fda213872f85b4f48
yogmis <cool71661gmail.com>
2019-08-31 15:47:10 +0530

    this is a new file

---
+++ JFrame.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.awt.*;
+public class JFrame {
+
+    public static void main(String[] args) {
+	Frame F =new Frame("My AWR Frame");
+	F.setSize(300,250);
+	F.setVisible(true);
+
+    }
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Jframeapp
-this testing git
